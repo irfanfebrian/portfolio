9887514fa2535633797a6b88202223fc3709c616
irfanfebrian <irfanfebriaan@gmail.com>
2025-07-29 13:28:15 +0700

    Update base.php

--- require/base.php
+++ require/base.php
@@ -1,5 +1,5 @@
 <?php
-	define('BASE_URL','https://portfolio-irfanf.wasmer.app/');
+	define('BASE_URL','https://portfolio-irfanf.wasmer.app');
 	function site_url ( $path = '')
 	{
 		if( is_array( $path ))
